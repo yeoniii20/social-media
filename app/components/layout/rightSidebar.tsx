117ba1cae61b93e47f05f3c867812e23c024b06b
yeoniii20 <commetoi_yeoni@naver.com>
2025-09-14 02:01:33 +0900

    design: Create categoryList component

--- app/components/layout/rightSidebar.tsx
+++ app/components/layout/rightSidebar.tsx
@@ -2,11 +2,16 @@
 
 import React from 'react';
 import SuggestionList from '../list/suggestionList';
+import CategoryList from '../list/categoryList';
 
 const RightSidebar = () => {
   return (
     <div className='h-screen w-80 overflow-y-auto border-l border-border p-6 backdrop-blur-md no-scrollbar'>
-      <SuggestionList />
+      <h2 className='mb-4 mt-1 text-18b text-text-primary'>친구 추천</h2>
+      <div className='flex flex-col gap-12'>
+        <SuggestionList />
+        {/* <CategoryList /> */}
+      </div>
     </div>
   );
 };
