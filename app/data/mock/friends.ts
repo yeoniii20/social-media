23ac378fa31cccfc4a56f9b3f3bd1211de64a0e4
yeoniii20 <commetoi_yeoni@naver.com>
2025-09-14 00:12:19 +0900

    design: Create suggestionList component

---
+++ app/data/mock/friends.ts
@@ -0,0 +1,33 @@
+import friend1 from '@/app/images/icon/friends/friend1.png';
+import friend2 from '@/app/images/icon/friends/friend2.png';
+import friend3 from '@/app/images/icon/friends/friend3.png';
+import friend4 from '@/app/images/icon/friends/friend4.png';
+import friend5 from '@/app/images/icon/friends/friend5.png';
+import friend6 from '@/app/images/icon/friends/friend6.png';
+import friend7 from '@/app/images/icon/friends/friend7.png';
+import friend8 from '@/app/images/icon/friends/friend8.png';
+import friend9 from '@/app/images/icon/friends/friend9.png';
+import friend10 from '@/app/images/icon/friends/friend10.png';
+import friend11 from '@/app/images/icon/friends/friend11.png';
+import friend12 from '@/app/images/icon/friends/friend12.png';
+
+export const FRIENDS_SUGGESTION = [
+  {
+    id: 1,
+    name: '이창호',
+    mutual: '함께 아는 친구 10명',
+    profileImg: friend1,
+  },
+  {
+    id: 2,
+    name: '정승민',
+    mutual: '함께 아는 친구 8명',
+    profileImg: friend2,
+  },
+  {
+    id: 3,
+    name: '김지민',
+    mutual: '함께 아는 친구 15명',
+    profileImg: friend3,
+  },
+];
